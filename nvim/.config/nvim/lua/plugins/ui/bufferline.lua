f6a4cfb5b18021f85d825732aed5fc8d09bf244f
Oudeys <mattia.fait@gmail.com>
2025-04-16 18:27:06 +0200

    nvim: removed one plugin temporarily

--- nvim/.config/nvim/lua/plugins/ui/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/ui/bufferline.lua
@@ -1,25 +1,26 @@
-return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("bufferline").setup {
-            options = {
-                numbers = "ordinal", -- Mostra numeri sulle schede
-                diagnostics = "nvim_lsp", -- Mostra errori LSP sulle schede
-                separator_style = "slant", -- Stile del separatore
-                always_show_bufferline = true, -- Mostra sempre la barra delle schede
-                show_close_icon = false, -- Nasconde l'icona di chiusura globale
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        highlight = "Directory",
-                        separator = true,
-                    }
-                },
-            }
-        }
-    end
-}
-
-
+-- return {
+--     "akinsho/bufferline.nvim",
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--     config = function()
+--         require("bufferline").setup {
+--             options = {
+--                 numbers = "ordinal", -- Mostra numeri sulle schede
+--                 diagnostics = "nvim_lsp", -- Mostra errori LSP sulle schede
+--                 separator_style = "slant", -- Stile del separatore
+--                 always_show_bufferline = true, -- Mostra sempre la barra delle schede
+--                 show_close_icon = false, -- Nasconde l'icona di chiusura globale
+--                 offsets = {
+--                     {
+--                         filetype = "NvimTree",
+--                         text = "File Explorer",
+--                         highlight = "Directory",
+--                         separator = true,
+--                     }
+--                 },
+--             }
+--         }
+--     end
+-- }
+--
+--
+return{}
